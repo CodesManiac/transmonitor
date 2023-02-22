d787ead53267321fb6bc9d321bcbcca05d00eb57
CodesManiac <joyajiboye2016@gmail.com>
2023-02-23 00:29:41 +0100

    add sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { mainData, paymentsData, ordersData } from '../data/Data';
+import { MerchantProfile } from '../assets/icons';
 
 const Sidebar = () => {
-  return <aside className='py-6 text-navigationGrey'>Sidebar</aside>;
+  return (
+    <aside className='py-6 text-navigationGrey px-4'>
+      <button className='bg-green rounded-full text-white font-Roboto font-bold tracking-wide py-2 px-4 leading-[14px] text-xs'>
+        GENERATE INVOICE
+      </button>
+      <div className='mt-6'>
+        <h1 className='font-medium mb-6'>Main</h1>
+
+        {mainData.map((main) => (
+          <div
+            key={main.id}
+            className='flex gap-8 mb-6 cursor-pointer items-center'
+          >
+            <img src={main.icon} alt={main.title} />
+            <a href={main.route} className='text-[11px] leading-[14.63px]'>
+              {main.title}
+            </a>
+          </div>
+        ))}
+      </div>
+
+      <div className='mt-6'>
+        <h1 className='font-medium mb-6'>Payments</h1>
+
+        {paymentsData.map((payment) => (
+          <div
+            key={payment.id}
+            className='flex items-center gap-8 mb-6 cursor-pointer '
+          >
+            <img src={payment.icon} alt={payment.title} />
+            <a href={payment.route} className='text-[11px] leading-[14.63px]'>
+              {payment.title}
+            </a>
+          </div>
+        ))}
+      </div>
+      <div className='mt-6  '>
+        <h1 className='font-medium mb-6'>Orders</h1>
+        {ordersData.map((order) => (
+          <div key={order.id} className='flex gap-8 cursor-pointer mb-6'>
+            <img src={order.icon} alt={order.title} />
+            <a href={order.route} className='text-[11px] leading-[14.63px]'>
+              {order.title}
+            </a>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <div className='flex gap-8 cursor-pointer'>
+          <img src={MerchantProfile} alt='Merchant' />
+          <p className='text-[11px] leading-[14.63px]'>Merchant Profile</p>
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const OverView = () => {
-  return <section className='p-8'></section>;
+  return <section className='p-8'>Overview </section>;
 };
 
 export default OverView;
