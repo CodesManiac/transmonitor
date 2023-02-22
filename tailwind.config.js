899531b412e8e6f8d904fbc7057ae06d4fcf58a2
CodesManiac <joyajiboye2016@gmail.com>
2023-02-22 23:29:22 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      green: '#27AE60',
+      navigationGrey: '#647787',
+      white: '#FFFFFF',
+      offWhite: '#F4F4F4',
+      outletBackground: '#F7F8FA',
+      grey: '#787C90',
+      dark: '#262626',
+      ash: '#4F4F4F',
+      black: '#000000',
+      yellow: '#EBC315',
+      blue: '#1860EC',
+      blu: '#1875F0',
+      deepGrey: '#414042',
+      searchBorder: '#787878',
+      // statusBorder: '#CEDODA',
+      tableHead: '#EAEEFO',
+      tableData: '#7F8FA4',
+      tableRowBorder: '#CCCFD4',
+      transparent: 'transparent',
+    },
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif'],
+      SegoeUI: ['Segoe UI', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'graph': "url('../src/assets/images/big-graph.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
